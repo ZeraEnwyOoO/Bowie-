e8285fc888342a76ff9063cf17582500608a94be
wWyo_O <zingert03@gmail.com>
2026-09-17 20:48:27 +0700

    Update hashmap.c

--- core/src/util/hashmap.c
+++ core/src/util/hashmap.c
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+#define _POSIX_C_SOURCE 200809L
 
 #include "wingo/util/hashmap.h"
 
